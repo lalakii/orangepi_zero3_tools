8d4a7e5f4e5a6abe638272b811c0998102c359d7
拉拉姬 <31397186+lalakii@users.noreply.github.com>
2023-10-05 19:23:03 +0800

    Update json_data.sh

--- json_data.sh
+++ json_data.sh
@@ -31,7 +31,7 @@ sr() {
         fi
     done
     core=$value
-    mem=$(free | grep -i mem | awk '{print "["$3","$4"]"}')
+    mem=$(free | grep -i mem | awk '{print "["$3","$2"]"}')
     cat <<EOF
 {
         "sensors":{
@@ -47,23 +47,34 @@ EOF
 
 info() {
     eval "$(cat /etc/os-release)"
-    host=\"$(hostname)\"
-    ip=[\"$(hostname -I | sed "s/ /\",\"/g" | sed 's/,\"*$//')]
-    disk=$(df / | tail -n1 | awk '{print "["$3","$4"]"}')
-    krv=\"$(uname -or)\"
-    arch=\"$(uname -m)\"
-    boot_time=\"$(uptime -s)\"
-    sys_time=\"$(date +'%Y-%m-%d %H:%M:%S')\"
+    host=$(hostname)
+    krv=$(uname -or)
+    arch=$(uname -m)
+    ip=$(hostname -I | sed "s/ /\",\"/g" | sed 's/,\"*$//')
+    disk=$(df / | tail -n1 | awk '{print "["$3","$2"]"}')
+    rootfs=$(mount | grep " / " | awk '{print $1}')
+    eval "$(blkid | grep "$rootfs" | awk -F ':' '{print $2}')"
+    fsck_info=$(tune2fs -l "$rootfs" | grep -i "state\|Mount count" | awk -F':' '{gsub(/[ \t]+/, "", $2); print $2}')
+    fs_state=$(echo "$fsck_info" | head -n1 | tail -n1)
+    mount_count=$(echo "$fsck_info" | head -n2 | tail -n1)
+    mount_max_count=$(echo "$fsck_info" | tail -n1)
+    boot_time=$(uptime -s)
+    sys_time=$(date +'%Y-%m-%d %H:%M:%S')
     cat <<EOF
 {
-    "host":${host},
+    "host":"${host}",
     "os":"${PRETTY_NAME}",
-    "kernel":${krv},
-    "machine":${arch},
-    "ip":${ip},
+    "kernel":"${krv}",
+    "machine":"${arch}",
+    "ip":["${ip}],
     "disk":${disk},
-    "boot_time":${boot_time},
-    "sys_time":${sys_time}
+    "rootfs":"${rootfs}",
+    "rootfstype":"${TYPE:-default}",
+    "fs_state":"${fs_state}",
+    "mount_count":${mount_count},
+    "max_count":${mount_max_count},
+    "boot_time":"${boot_time}",
+    "sys_time":"${sys_time}"
 }
 EOF
 }
@@ -78,4 +89,5 @@ case $1 in
     echo "$0 [ state | info ]"
     ;;
 esac
+
 ## lalaki.cn ##
